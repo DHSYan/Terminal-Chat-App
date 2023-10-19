d3333bdf69954f05e82b3dd0580eaf6eb8a8065f
DHSYan <sean.yan@student.unsw.edu.au>
2023-10-19 11:43:52 +1100

    Separation between server and client

---
+++ client/main.c
@@ -0,0 +1,20 @@
+// This is a chat app program coded for the Course COMP3331 at UNSW
+// This is the client app
+// Author: Sean Yan
+// zID: z5521807
+//
+// Acknowledegment
+// All Code borrowed or taken from other resources will be comment before the 
+// function or block section
+//
+//
+
+
+// Stdlib
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    return 0;
+}
